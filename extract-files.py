4ce2f0324e9e32e480c4fcbcec12ed76c3b5d537
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-08-03 06:14:33 +0000

    vitamin: Link libaudioclient_shim to libsink-mtk
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -52,6 +52,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('start', 'enable'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
     (
